219eee3c18821f4403217d4ff378ae78afac0294
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-07-07 12:04:34 +0530

    tidy up tests

--- flagsmith/provider_test.go
+++ flagsmith/provider_test.go
@@ -1,7 +1,6 @@
 package flagsmith_test
 
 import (
-	"github.com/Flagsmith/flagsmith-go-api-client"
 	"github.com/Flagsmith/terraform-provider-flagsmith/flagsmith"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -10,21 +9,23 @@ import (
 	"testing"
 )
 
+// Create provider factories - to be used by resource tests
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"flagsmith": providerserver.NewProtocol6WithError(flagsmith.New("")()),
 }
 
-func mustHaveEnv(t *testing.T, name string) {
-	if os.Getenv(name) == "" {
-		t.Fatalf("%s environment variable must be set for acceptance tests", name)
-	}
-}
 func testAccPreCheck(t *testing.T) {
 	mustHaveEnv(t, "FLAGSMITH_MASTER_API_KEY")
 	mustHaveEnv(t, "FLAGSMITH_FEATURE_NAME")
 	mustHaveEnv(t, "FLAGSMITH_ENVIRONMENT_KEY")
 	mustHaveEnv(t, "FLAGSMITH_ENVIRONMENT_ID")
 	mustHaveEnv(t, "FLAGSMITH_FEATURE_ID")
+}
+
+func mustHaveEnv(t *testing.T, name string) {
+	if os.Getenv(name) == "" {
+		t.Fatalf("%s environment variable must be set for acceptance tests", name)
+	}
 }
 
 func masterAPIKey() string {
@@ -50,13 +51,4 @@ func featureID() int {
 		panic(err)
 	}
 	return v
-}
-
-var tc *flagsmithapi.Client
-
-func testClient() *flagsmithapi.Client {
-	if tc == nil {
-		tc = flagsmithapi.NewClient(masterAPIKey(), "http://localhost:8000/api/v1")
-	}
-	return tc
 }
