e75db54ef1d7c13d06d3cc83a4519274a96f9e76
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-07-07 14:46:55 +0530

    typo

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,4 +1,4 @@
 provider "flagsmith" {
-  # or omit this for maser_api_key to be read from environment variable
+  # or omit this for master_api_key to be read from environment variable
   master_api_key = "<Master API Key>"
 }
