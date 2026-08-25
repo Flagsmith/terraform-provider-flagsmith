4f96a7301680d526a358f6cebb1937db4b21dfb7
germangarces <german.garces@flagsmith.com>
2026-08-25 10:29:29 +0200

    docs
    Signed-off-by: germangarces <german.garces@flagsmith.com>

--- examples/data-sources/flagsmith_project/data-source.tf
+++ examples/data-sources/flagsmith_project/data-source.tf
@@ -2,7 +2,7 @@ data "flagsmith_project" "my_project" {
   uuid = "10421b1f-5f29-4da9-abe2-30f88c07c9e8"
 }
 
-resource "flagsmith_feature" "my_feature" {
-  feature_name = "my_feature"
-  project_uuid = data.flagsmith_project.my_project.uuid
+resource "flagsmith_environment" "staging" {
+  name       = "Staging"
+  project_id = data.flagsmith_project.my_project.id
 }
